6652c501e2349054480c6540a3d37689bc904ca0
José Mata <josemata@Joses-MacBook-Pro.local>
2024-02-04 19:37:17 -0500

    seccond commit

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # josemataHomework1
+
+This is Jose Amilcar Mata Calidonio's homework assignment #1 for the COMP 510 course. 
+
+The instructions for this assignment are shown below:
+
+Homework 1 : counting words
+(in your class/homework grade this time)
+
+
+Summary:
+write a word counting program with go using at least three functions
+
+
+Due: Sunday  Feb 4th by 11:59pm
+
+
+get War and Peace as a text file https://www.gutenberg.org/files/2600/2600-0.txt
+
+
+
+Details:
+
+Be sure to create a new project with your name somewhere in the project name. (I might use jsantoreHomework1)
+
+
+Write this in at least three functions
+
+
+
+main should ask the user for the file name, and then open the file and convert it to a slice of strings using strings.Split then pass the slice of strings to the next functions
+countWords should take a slice of strings as its parameter and then count the number of times each word occurs (using a map), it should return the map containing the word counting
+finally reportResults should take a map of string to int (the word count map) and print out how often each word occurs in the file. One word count per line.
+
+
+Note that since main needs to ask the user for a file, I can (and will) try multiple files when grading.
+
+
+To get rid of punctuation use something like
+
+
+// Make a Regex to say we only want letters and numbers
+reg, err := regexp.Compile("[^a-zA-Z0-9]+")
+if err != nil {
+log.Fatal(err)
+}
+processedString := reg.ReplaceAllString(example, "")
+
+
+Submitting the project.
+
+submit the project by putting it on github and sending me a collaboration invite. (I'm git user jsantore)
+
+
+if you need to min/max, here is how I'll grade
--- josemataHomework1.go
+++ josemataHomework1.go
@@ -7,62 +7,54 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 )
 
 func main() {
 	fmt.Print("Welcome Jose Mata's word counter program.\nPlease enter the name of the text file you want to use (followed by .txt):")
-	//Create string called nameoffile and save the user's input to it. This will be the name of the txt file to open.
+	//Create string called nameoffile and save the user's input to it. This will be the name of the .txt file to open.
 	var nameoffile string
 	fmt.Scanln(&nameoffile)
 	fmt.Println("The name of the file is:", nameoffile)
 	/*
-		Open up the file and store the contents of the file into a string called content, else it will print out the byte data.
-		The if err loop checks for errors.
-		The string is then split into slices and stored as "allwords". I was originally going to use strings.Split but instead I decided to use
-		strings.Fields so that a new line would also be used as a delimeter.
+		The following two lines of code open up the .txt file and store the contents of the file into a string called content, so it is no longer handled as byte data.
+		The if statement on Line 25 checks for errors when opening the .txt file.
 	*/
-	content, err := os.ReadFile(nameoffile)
+	content, err := os.ReadFile(nameoffile) //This line of code was taken from the code used in our in-class Go program created during the lecture on January 29, 2024 and modified to be used in this program.
 	if err != nil {
 		log.Fatal("Help file error", err)
-	}
+	} //This line of code was taken from the code used in our in-class Go program created during the lecture on January 29, 2024 and modified to be used in this program.
 	str := string(content)
-	allwords := strings.Fields(str)
+	/*
+		The following five lines of code were taken from the instructions of this assignment and modified to be used in this program.
+	*/
+	reg, err := regexp.Compile("[^a-zA-Z0-9]+") //This line creates a regular expression that matches anything that isn't a lower case letter, a capital letter, or a number.
+	if err != nil {                             //This if statement checks for errors when create the regular expression
+		log.Fatal(err)
+	}
+	processedString := reg.ReplaceAllString(str, " ") /*This line goes through the str string and replaces anything
+	that matches the reg regular expression with a space and stores the new string in the string called processedString.*/
+	/*
+		The string is then split into a slice and stored as "allwords". I was originally going to use strings.Split but instead I decided to use
+		strings.Fields so that multiple spaces and/or a new line would also be used as a delimeter.
+	*/
+	allwords := strings.Fields(processedString)
 	countWords(allwords) //Call the function countWords with allwords as the paramenter slice
 }
 
 func countWords(sliceofwords []string) {
-	wordmap := make(map[string]int)      //create an empty map
+	wordmap := make(map[string]int)      //create an empty map called wordmap
 	for _, value := range sliceofwords { //for each word in the sliceofwords slice
-		wordmap[value]++ //increase by one the count of each word (increase the value of the key by one)
+		wordmap[value]++ /*increase by one the count of each word (increase the value of the key by one).
+		The keys of the map will be automatically created if there isn't one made for the word yet and will start with a value of 0.*/
 	}
-	reportResults(wordmap)
+	reportResults(wordmap) //call the function reportResults and pass the map wordmap as the parameter
 }
 
-func reportResults(wordmap map[string]int) {
-	fmt.Println("The following is a list of each word followed by the amount of times it appears:")
-	for k, v := range wordmap {
-		fmt.Println(k, ":", v)
+func reportResults(results map[string]int) {
+	fmt.Println("The following is a list of each word followed by the amount of times it appears in the .txt file:")
+	for k, v := range results { //for each key-value pair in the map results
+		fmt.Println(k, ":", v) //print first the key (the word), then a colon, then the value (number of word counts) and continue in a new line
 	}
 }
-
-/*
-	loop to print out the number of words in the slice allwords
-	for index, value := range allwords {
-		fmt.Println(index, value)
-	}
-*/
-
-/*
-	In case the professor doesn't let me use strings.Fields:
-	There are two separators that I want to use: a space and a new line, so that every word (each substring) is its own element in the slice.
-	To achieve this spliting with two separators, I used the "iterative splitting" method, which is based on the code found in this website:
-	https://marketsplash.com/tutorials/go/golang-split-string/
-
-	delimiters := []string{" ", "\n"} //I'm creating a slice that contains the delimiters that I want to use to split the string
-	for _, delimiter := range delimiters { //For each delimiter in the delimiters slice
-		segments := strings.Split(str, delimiter)   //In the segments slice, include the substring separated by the delimiter
-		str = strings.Join(segments, " ")
-	}
-	for i := 0; i <len(Allwords);
-*/
